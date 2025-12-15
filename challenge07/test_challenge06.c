e8125084220f134216a22ba1d0454770e3f55a37
TwikiTuki <lo.poponutu@gmail.com>
2025-12-15 19:11:43 +0100

    challenge07

---
+++ challenge07/test_challenge06.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <assert.h>
+
+
+int suma(int a, int b);
+int resta(int a, int b);
+int multiplicacion(int a, int b);
+int division(int a, int b);
+
+int main()
+{
+    assert(suma(2,3) == 5);
+    assert(suma(-2,3) == 1);
+    assert(suma(2,-3) == -1);
+    assert(suma(-2,-3) == -5);
+
+    assert(resta(2,3) == -1);
+    assert(resta(-2,3) == -5);
+    assert(resta(2,-3) == 5);
+    assert(resta(-2,-3) == 1);
+
+    assert(multiplicacion(2,3) == 6);
+    assert(multiplicacion(-2,3) == -6);
+    assert(multiplicacion(2,-3) == -6);
+    assert(multiplicacion(-2,-3) == 6);
+
+    assert(division(9,3) == 3);
+    assert(division(-12,3) == -4);
+    assert(division(9,-3) == -3);
+    assert(division(-12,-3) == 4);
+    // assert(0);
+    printf("ALL TESTS PASSED\n");
+}
